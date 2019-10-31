582ff71ba4fe773361054fbb7344325e1e57e263
r475park <57232066+r475park@users.noreply.github.com>
2019-10-31 11:44:34 -0600

    Create secondLargest.cpp

---
+++ secondLargest.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int num1 = 0,num=0,even=0,odd=0,a=0,b=0;
+	while(num != -1){
+		cout<<"Enter a number: ";
+		cin>>num;
+		if(num % 2 == 0){
+			even++;
+		}else if(num %2 == 1) {
+			odd++;
+		}
+		if(num>a){
+			b = a;
+		} else if(num <= b && num != a){
+			b = num;
+		}
+		if(num>a){
+			a=num;
+		}
+	cout<<endl;
+	cout<<"Largest Number: "<<a<<endl;
+	cout<<"Second largest: "<<b<<endl;
+	cout<<"Even: "<<even<<endl;
+	cout<<"Odd: "<<odd<<endl;
+	cout<<endl;
+	}
+	return 0;
+}
