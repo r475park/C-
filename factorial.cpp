25dd4b68c490cfeb60516163baf23463c44660b4
r475park <57232066+r475park@users.noreply.github.com>
2019-10-31 11:43:11 -0600

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int factorial=1,num;
+	cout<<"Enter a number: : ";
+	cin>>num;
+	cout<<endl;
+	for(int i=1;i<num;i++){
+		factorial = factorial*i;
+	}
+	cout<<"The Factorial of "<<num<<" is: "<<factorial<<endl;
+	return 0;
+}
